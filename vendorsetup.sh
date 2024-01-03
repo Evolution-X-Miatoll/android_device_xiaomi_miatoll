725dd5907d9da00bfc96b31e71d582bba774f7f0
Sonal Singh <sonal.singh.19993@gmail.com>
2024-01-04 00:50:13 +0530

    miatoll: Add vendorsetup.sh
    Signed-off-by: Sonal Singh <sonal.singh.19993@gmail.com>

---
+++ vendorsetup.sh
@@ -0,0 +1,6 @@
+git clone https://github.com/Evolution-X-Devices/kernel_xiaomi_sm6250.git -b udc kernel/xiaomi/sm6250
+git clone https://github.com/Evolution-X-Devices/device_xiaomi_sm6250-common.git -b udc device/xiaomi/sm6250-common
+git clone https://github.com/Evolution-X-Devices/vendor_xiaomi_miatoll.git -b udc vendor/xiaomi/miatoll
+git clone https://github.com/Evolution-X-Devices/vendor_xiaomi_sm6250-common.git -b udc vendor/xiaomi/sm6250-common
+git clone https://gitlab.com/SonalSingh18/vendor_xiaomi_miuicamera.git -b fourteen vendor/xiaomi/miuicamera
+git clone https://github.com/TogoFire/packages_apps_ViPER4AndroidFX.git -b v4a packages/apps/ViPER4AndroidFX
